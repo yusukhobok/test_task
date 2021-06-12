de3940692a58e8855cd520d42e8763f7d224c321
Yu. Sukhobok <khvyus@gmail.com>
2021-06-12 21:25:50 +1000

    add basic calculations

---
+++ src/calculations.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+class PhasePattern:
+    def __init__(self, options):
+        self.fi_s = np.radians(options["fi_s_deg"])
+        self.theta_s = np.radians(options["theta_s_deg"])
+        self.fi_0 = np.radians(options["fi_0_deg"])
+        self.theta_0 = np.radians(options["theta_0_deg"])
+        self.l_main = options["l_main"]
+        self.l_side = options["l_side"]
+
+        self.fi_min = np.radians(options["fi_min_deg"])
+        self.fi_max = np.radians(options["fi_max_deg"])
+        self.theta_min = np.radians(options["theta_min_deg"])
+        self.theta_max = np.radians(options["theta_max_deg"])
+        self.fi_count = options["fi_count"]
+        self.theta_count = options["theta_count"]
+
+        self.fi_array = None
+        self.theta_array = None
+        self.fi_grid = None
+        self.theta_grid = None
+
+        self.DNA = None
+
+        self._fill_angle_arrays()
+
+    def _calc_l(self, fi, theta):
+        L = np.empty((self.fi_count, self.theta_count))
+        L.fill(self.l_side)
+        cond1 = np.abs(fi - self.fi_s) < self.fi_0 / np.cos(self.fi_s)
+        cond2 = np.abs(theta - self.theta_s) < self.theta_0 / np.cos(self.theta_s)
+        L[cond1 & cond2] = self.l_main
+        return L
+
+    def _calc_G(self, fi, theta):
+        frac1 = np.pi * (fi - self.fi_s) * np.cos(self.fi_s) / (2 * self.fi_0)
+        frac2 = np.pi * (theta - self.theta_s) * np.cos(self.theta_s) / (2 * self.theta_0)
+        L = self._calc_l(fi, theta)
+        return np.abs(np.cos(frac1) * np.cos(frac2)) * L
+
+    def _fill_angle_arrays(self):
+        self.fi_array = np.linspace(self.fi_min, self.fi_max, self.fi_count)
+        self.theta_array = np.linspace(self.theta_min, self.theta_max, self.theta_count)
+
+    def calc_DNA(self):
+        self.fi_grid, self.theta_grid = np.meshgrid(self.fi_array, self.theta_array)
+        self.DNA = self._calc_G(self.fi_grid, self.theta_grid)
+
---
+++ src/main.py
@@ -0,0 +1,55 @@
+# import matplotlib
+# matplotlib.use('TKAgg')
+
+from calculations import PhasePattern
+import matplotlib.pyplot as plt
+import numpy as np
+
+def test():
+    options = {
+        "fi_s_deg": 30.,
+        "theta_s_deg": 20.,
+        "fi_0_deg": 10.,
+        "theta_0_deg": 10.,
+        "l_main": 10.,
+        "l_side": 1.,
+        "fi_count": 1000,
+        "theta_count": 1000,
+        "fi_min_deg": -60.,
+        "fi_max_deg": 60.,
+        "theta_min_deg": -50.,
+        "theta_max_deg": 50.
+    }
+    
+
+
+    phase_pattern = PhasePattern(options)
+    phase_pattern.calc_DNA()
+
+    limits = [phase_pattern.fi_min, phase_pattern.fi_max, phase_pattern.theta_max, phase_pattern.theta_min]
+    limits_deg = np.rad2deg(limits)
+
+    # plt.imshow(phase_pattern.DNA, extent=limits_deg)
+    # plt.colorbar()
+
+    # delta_fi = phase_pattern.fi_array[1] - phase_pattern.fi_array[0]
+    # delta_theta = phase_pattern.theta_array[1] - phase_pattern.theta_array[0]
+    # index_fi = int((phase_pattern.fi_s - phase_pattern.fi_min) // delta_fi)
+    # index_theta = int((phase_pattern.theta_s - phase_pattern.theta_min) // delta_theta)
+    # plt.plot(phase_pattern.fi_array, phase_pattern.DNA[:,index_theta])
+    # plt.plot(phase_pattern.theta_array, phase_pattern.DNA[index_theta, :])
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(phase_pattern.fi_grid, phase_pattern.theta_grid, phase_pattern.DNA)
+
+    plt.show()
+
+
+
+
+
+
+if __name__ == "__main__":
+    test()
